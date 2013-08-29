2fc00aff6aaf3897db6df26d1825ad307293f046
DOGGY <user@user-PC.(none)>
2013-08-29 09:55:10 +0800

    Initial commit

---
+++ web.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+string = fs.readFileSync('index.html');
+buf = new Buffer(string);
+
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+    response.send(buf.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
