40e6c4cab04dd4abe3e3d76f47d8bb06111a1693
sctech <atfrontier@hotmail.com.tw>
2013-08-29 19:38:05 +0800

    successful one

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ app.get('/', function(request, response) {
     response.send(buf.toString());
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
